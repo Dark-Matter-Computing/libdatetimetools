be39f77ce57b748da02370c78de01ef263295510
THV <thomashvidal@gmail.com>
2021-01-11 23:13:38 -0800

    Initiate Error Handling Module
    Create errorhandler_pvt.h header for error handling. Assumption is that
    the error handling module will be completely private to users of the
    library. Create errorhandler.c for implementing the error handling
    functions. "Outlined" the organization of the error handling system.

---
+++ src/errorhandler.c
@@ -0,0 +1,74 @@
+/*
+ * Filename: errorhandler.c
+ * Library: libdatetimetools
+ *
+ * Description: libdatetimetools is a library of useful functions to enable
+ * users to perform date and time calculations. One of its primary uses is as a
+ * component of solutions to computing chain-based scheduling, as in litigation
+ * and project management. 
+ *
+ * Version: See version.h
+ * Created: 01/11/2021 23:04:50
+ * Last Modified: Mon Jan 11 23:06:18 2021
+ *
+ * Author: Thomas H. Vidal (THV), thomashvidal@gmail.com
+ * Organization: Dark Matter Computing
+ *
+ * Copyright: (c) 2011-2020 - Thomas H. Vidal, Los Angeles, CA
+ * SPDX-License-Identifier:  LGPL-3.0-only
+ *
+ * Usage: 
+ * File Format: 
+ * Restrictions: 
+ * Error Handling: 
+ * References: 
+ * Author: Thomas H. Vidal (THV), thomashvidal@gmail.com
+ * Notes: 
+ */
+
+
+/*-----------------------------------------------------------------------------
+ * Error Handling  
+ *----------------------------------------------------------------------------*/
+
+/* ----- Computation Error Handling ----- */
+
+    /*  MARK - Computation errors */
+
+
+/* ----- File i/o Error Handling ----- */
+
+    /* Mark - File i/o */
+
+
+/* ----- Configuration Error Handling ----- */
+
+    /* Mark - Configuration */
+
+
+/* ----- Memory Error Handling ----- */
+
+    /* Mark - Memory */
+
+
+/* ----- User Interface Error Handling ----- */
+
+    /* Mark - User Interface */
+
+
+/*-----------------------------------------------------------------------------
+ * Error Reporting  
+ *----------------------------------------------------------------------------*/
+
+/* ----- CLI Non-Interactive Mode Error Reporting ----- */
+
+    /* Mark CLI - Non-Interactive */
+
+/* ----- CLI Interactive Mode Error Reporting ----- */
+
+    /* Mark - CLI Interactive Mode */
+
+/* ----- GUI Mode Error Reporting ----- */
+
+    /*  Mark - GUI Mode */
+
